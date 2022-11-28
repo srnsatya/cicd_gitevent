09ef3ecbbc576202525c160023c9f714a7a8b84f
srnsatya <bestsatyam@gmail.com>
2022-11-27 21:58:32 -0500

    modified for port and requestmapping

--- src/main/java/com/cicd/gitaction/GitActionApplication.java
+++ src/main/java/com/cicd/gitaction/GitActionApplication.java
@@ -3,10 +3,12 @@ package com.cicd.gitaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController("/jt")
+@RestController
+@RequestMapping("/jt")
 public class GitActionApplication {
 
 	@GetMapping("/cicdgitaction")
